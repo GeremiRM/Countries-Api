6c9f8f4b0c01af2281512cb75051393e56247302
Geremi Ramos <geremiramosm@gmail.com>
2021-11-05 12:32:40 -0400

    "Initializing project"

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => (
+  <div>
+    <div></div>
+  </div>
+);
+
+export default Index;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,19 +1,27 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  xl: "80em",
+});
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
+
+  config,
+
   fonts,
   breakpoints,
   icons: {
@@ -33,9 +41,9 @@ const theme = extendTheme({
           />
         </svg>
       ),
-      viewBox: '0 0 3000 3163',
+      viewBox: "0 0 3000 3163",
     },
   },
-})
+});
 
-export default theme
+export default theme;
